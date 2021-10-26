1b2e011c386ba0404ef3cb7770f4c9620db44160
GG <Ghunther0111@gmail.com>
2021-10-26 05:19:56 -0400

    random sale algorithm

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get("/full-db", (req, res) =>{
     giveAllListings(res)
-})
+});
 app.get(("/"), (req, res) =>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
