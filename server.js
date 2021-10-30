a0e1ba928bf383769f3d34955459e791d690836c
GG <Ghunther0111@gmail.com>
2021-10-30 04:04:43 -0400

    random sale algorithm

--- server.js
+++ server.js
@@ -63,6 +63,8 @@ const timeCheck = () => {
                                 console.log("bing")
                                 if (err)
                                     return console.error(err);
+                        }).catch(()=>{
+                            console.log("Update rejected! This is causing delay")
                         })
                     }
                     catch (err) {
