b3db0c540a075db41c155a9708288ad0f625e36f
GG <Ghunther0111@gmail.com>
2021-11-01 06:03:37 -0400

    random sale algorithm

--- src/components/fullStock.js
+++ src/components/fullStock.js
@@ -18,6 +18,9 @@ class FullList extends Component {
         this.imgMap = this.imgMap.bind(this)
     }
     componentDidMount() {
+        /*this axios function calls my /full-db REST API then passes the JSON to a nameless 
+        function which puts the relevant data into the state to be referenced every time the
+        web page is opened*/
         Axios.get("/full-db").then((response) => {
             var nameList = []
             var picList = []
@@ -44,27 +47,19 @@ class FullList extends Component {
             })
         });
     }
-    imgMap() {
+    listingsMap() {
         /*zipper turns the state into an array of arrays where each one represents
         a listing to be displayed on the webpage. */
-        let zipper = this.state.names.map((name, i) => [name, this.state.pics[i], this.state.ogPrices[i], this.state.disPrices[i], this.state.ids[i], this.state.sales[i]]);
-        if (zipper[5]){
-            return (
-                <div>
-                    {zipper.map((listing, i) => (
-                        <div className="feat-box col-2" key={i}>
-                            <a href={"/item?id=".concat(listing[4])} className="feat-link">
-                                <img src={listing[1]} alt="oopsies" className="feat-img" />
-                                <h4 className="feat-name">{listing[0]}</h4>
-                            </a>
-                            <h5 className="feat-price crossed">${listing[2]}</h5>
-                            <h5 className="feat-price">${listing[5]}</h5>
-                        </div>
-                    ))}
-                </div>
-            )
+        let zipper = this.state.names.map((name, i) => [name, this.state.pics[i], this.state.ogPrices[i], this.state.disPrices[i], this.state.ids[i]]);
+        let saleOutput =[]
+        let saleCheck = (i)=>{
+            if (this.state.sale[i]){
+                return "feat-price"
+            }
+            else{return "clear"}
         }
         return (
+            //maps the zipper array into individual UI elements to be rendered. 
             <div>
                 {zipper.map((listing, i) => (
                     <div className="feat-box col-2" key={i}>
@@ -73,6 +68,7 @@ class FullList extends Component {
                             <h4 className="feat-name">{listing[0]}</h4>
                         </a>
                         <h5 className="feat-price">${listing[2]}</h5>
+                        <h5 className={saleCheck(i)} >${listing[3]}</h5>
                     </div>
                 ))}
             </div>
@@ -83,7 +79,7 @@ class FullList extends Component {
             <div id="primary" className="container-fluid">
                 <div id="stockBody" className="row">
                     <div id="stock" className="col-10">
-                        {this.imgMap()}
+                        {this.listingsMap()}
                     </div>
                 </div>
             </div>
